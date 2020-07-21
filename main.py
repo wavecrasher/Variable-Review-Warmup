b5cb6b7e8e0f9dd470e7f9cbc3b859ebdf16d654
Hannah Evans <wavecrasherhge@gmail.com>
2020-07-21 17:01:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#Write a Python program which accpets the users first and last name and print them in reverse order with a space btwn them.
+
+#first_name = input("What is your first name?")
+#last_name = input("what is your last name?")
+#print("Hello " + last_name + first_name )
+
+
+
+#Creat a for loop that will increase a variable ny once
+
+counter = 0
+for i in range(3):
+  counter += 1
+  print(counter)
